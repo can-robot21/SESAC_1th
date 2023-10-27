4009aa261c3b59a8a6629219b2f25d5dc9fba7c0
can-robot21 <can.robot21@gmail.com>
2023-10-27 17:41:04 +0900

    add

--- 1.HTML/js/calcurator.js
+++ 1.HTML/js/calcurator.js
@@ -1,1 +1,24 @@
+let currentKey;
+let currentNum = '';
+let sumNum;
 
+function clearScreen() {
+    currentNum = document.querySelector('.screen');
+    currentNum.innerHTML = '';
+    break;
+}
+
+function addNum(num) {
+    currentNum += num;
+    document.querySelector('.screen').innerHTML = currentNum;
+}
+
+function operator(op) {
+    if (opertor !== '') {
+        preNum = currentNum;
+        currentNum = '';
+        opertor = op;
+        document.querySelector(.screen).classList.add(preNum)
+    
+    }
+}
