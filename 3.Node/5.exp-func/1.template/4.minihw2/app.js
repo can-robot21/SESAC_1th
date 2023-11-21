980d97d72ebda20123fc166e10985a768d2034be
can-robot21 <can.robot21@gmail.com>
2023-11-21 22:34:05 +0900

    +add express & 미들웨어

--- 3.Node/5.exp-func/1.template/4.minihw2/app.js
+++ 3.Node/5.exp-func/1.template/4.minihw2/app.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const nunjucks = require('nunjucks');
 const fs = require('fs');
-const csv = require('csv-parser');
+// const csv = require('csv-parser');
+const csv = require('fast-csv');
+
 const app = express();
 const port = 3000;
 
@@ -9,56 +11,93 @@ const port = 3000;
 nunjucks.configure('views', { express: app });
 app.set('view engine', 'html');
 
-// 라우터
-app.get('/', (req, res) => {
-    res.redirect('/index');
+// 퍼포먼트 성능 테스트
+app.use((req, res, next) =>{
+    const  start = Date.now();
+    res.on('finish', () => {
+        const end = Date.now();
+        const duration = end - start;
+
+        console.log(' 경과 시간 :', duration);
+    })
+    console.log('미들웨어');
+    next();
 })
 
-// nunjuck 
+// 정적파일 사용 폴더 설정
+app.use(express.static('public'));
 
-let userFile = './views/user.csv';
+//
+const data = [];
+const filenames = [];
 
-function readCsv(fileName) {
-    const results = [];
-    try {
-        // const data = fs.readFileSync(fileName, 'utf8');
-        // const parseData = csv.parse(data, {columns: true});
-        // console.log(parseData);
-        // return parseData;
-        fs.createReadStream(fileName)
-            .pipe(csv())
-            .on('data', (data) => results.push(data))
+async function loadDataIntoMemory() {
+    return new Promise((resolve, reject) => {
+        fs.createReadStream('./user.csv', {encoding: 'utf8'})
+            .pipe(csv.parse( {headers: true, trim: true})) // 미션2. 공백 처리 옵션 활용
+            .on('headers', (headers) => {
+                filenames.push(...headers);
+                console.log(filenames);
+            })
+            .on('data', (row) => {
+                data.push(row);
+                // 미션1. 공백이 들어간 데이타 저장.
+                // console.log(row);
+            })
             .on('end', () => {
-                console.log(results);
-                const users = results;
-                // 렌더 페이지 호출
-                renderPage(results);
-                // [
-                //   { NAME: 'Daffy Duck', AGE: '24' },
-                //   { NAME: 'Bugs Bunny', AGE: '22' }
-                // ]
-            });
-    } catch (err) {
-        console.log('파일 읽기 중 에러 발생');
-        throw err;
-    }
+                console.log('파일 읽기 완료');
+                resolve();
+            })
+            .on('error', (error) => {
+                console.log('오류', error);
+                reject(error);
+            })
+    })
+
+    
 }
 
-// 렌더페이지 
-function renderPage(users) {
-    const data = {
-        title:  "사용자 데이타",
-        users: users,
-    };
-    app.get('/index', (req, res) => {
-        res.render('index', data);
+// 서버시작
+
+async function startServer() {
+    await loadDataIntoMemory();
+    
+    app.get('/', (req, res) =>
+        // 데이터 저장
+        // 페이지
+        const itemsPerPage = 10;
+        let startIndex;
+        let endIndex;
+
+        console.log('요청:', req.query.page);
+
+        // 미션3. a테그
+        const page = req.query.page || 1;
+        startIndex = (page - 1) * itemsPerPage;
+        endIndex = startIndex + itemsPerPage;
+
+        //미션2. 전체 페이지수 계산
+
+        const totalPages = Math.ceil(data.length / itemsPerPage);
+        console.log(` 전체 ${data.length}, 페이지당 ${itemsPerPage}, 전체페이지 ${totalPages}`);
+
+        // 미션1. 페이지당 데이타
+    
+        const currPageRows = data.slice(startIndex, endIndex);
+
+        res.render('index', { 
+            title: "사용자 테이블",
+            data: currPageRows ,
+            total_Page: totalPages,
+            page: parseInt(page),
+            filenames: filenames
+        });
+    })
+    
+    // 서버
+    app.listen(port, () => {
+        console.log(`${port}에서 서버 시작`);
     });
 }
 
-const users = readCsv(userFile);
-console.log(userFile, users);
-
-// 서버
-app.listen(port, () => {
-    console.log(`${port}에서 서버 시작`);
-});
+startServer();
---
+++ 3.Node/5.exp-func/1.template/4.minihw2/app1.js
@@ -0,0 +1,69 @@
+const express = require('express');
+const nunjucks = require('nunjucks');
+const fs = require('fs');
+const csv = require('csv-parser');
+// const csv = require('fast-csv');
+
+const app = express();
+const port = 3000;
+
+// nunjucks 초기화
+nunjucks.configure('views', { express: app });
+app.set('view engine', 'html');
+
+// Express.js에서 정적 파일 제공 설정
+app.use(express.static('public'));
+
+// 라우터
+app.get('/', (req, res) => {
+    res.redirect('/index');
+})
+
+// nunjuck 
+
+let userFile = './user.csv';
+
+function readCsv(fileName) {
+    const results = [];
+    try {
+        // const data = fs.readFileSync(fileName, 'utf8');
+        // const parseData = csv.parse(data, {columns: true});
+        // console.log(parseData);
+        // return parseData;
+        fs.createReadStream(fileName)
+            .pipe(csv())
+            .on('data', (data) => results.push(data))
+            .on('end', () => {
+                console.log(results);
+                const users = results;
+                // 렌더 페이지 호출
+                renderPage(results);
+                // [
+                //   { NAME: 'Daffy Duck', AGE: '24' },
+                //   { NAME: 'Bugs Bunny', AGE: '22' }
+                // ]
+            });
+    } catch (err) {
+        console.log('파일 읽기 중 에러 발생');
+        throw err;
+    }
+}
+
+// 렌더페이지 
+function renderPage(users) {
+    const data = {
+        title:  "사용자 데이타",
+        users: users,
+    };
+    app.get('/index', (req, res) => {
+        res.render('index', data);
+    });
+}
+
+const users = readCsv(userFile);
+console.log(userFile, users);
+
+// 서버
+app.listen(port, () => {
+    console.log(`${port}에서 서버 시작`);
+});
