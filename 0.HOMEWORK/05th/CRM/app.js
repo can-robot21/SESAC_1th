c24e1bc03c1a36b3b571ae17605d6128fae71a9b
can-robot21 <can.robot21@gmail.com>
2023-12-06 05:02:29 +0900

    + 05th miniShop

--- 0.HOMEWORK/05th/CRM/app.js
+++ 0.HOMEWORK/05th/CRM/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const nunjucks = require('nunjucks');
 const sqlite3 = require('sqlite3');
 const ps = require('ps');
+const path = require('path');
 
 const app = express();
 const port = 3000;
@@ -10,15 +11,23 @@ const dbFile = "mydb.db";
 const db = new sqlite3.Database(dbFile);
 
 // nunjucks 설정
-nunjucks.configure("views", { express: app })
+const viewDir = path.join(__dirname, "views");
+const publicDir = path.join(__dirname, "public");
+
+nunjucks.configure(viewDir, {
+    express: app,
+    watch: true
+})
 app.set("view engine", "html");
 
+// // express.js 정적 파일 제공 설정
+// app.use(express.static("public"));
 // express.js 정적 파일 제공 설정
-app.use(express.static("public"));
+app.use(express.Router(publicDir));
 
 // 라우터
 app.get('/', (req, res) => {
-    res.redirect('/index', { title: "미니샵 관리화면", message: " 관리자 화면 "});
+    res.render('index', { title: "미니샵 관리화면", message: " 관리자 화면 " });
 })
 
 app.get('/user', (req, res) => {
@@ -34,7 +43,15 @@ app.get('/store', (req, res) => {
         title: "매장",
         content: "매장 리스트 + 검색"
     }
-    res.render('user', data);
+    res.render('store', data);
+})
+
+app.get('/item', (req, res) => {
+    const data = {
+        title: "상품",
+        content: "상품 리스트"
+    }
+    res.render('item', data);
 })
 
 app.get('/order', (req, res) => {
@@ -42,15 +59,15 @@ app.get('/order', (req, res) => {
         title: "주문 리스트",
         content: "주문 리스트 + 검색"
     }
-    res.render('user', data);
+    res.render('order', data);
 })
 
-app.get('/order', (req, res) => {
+app.get('/orderItem', (req, res) => {
     const data = {
-        title: "상품 리스트",
-        content: "상품 리스트 + 검색"
+        title: "주문상품 리스트",
+        content: "주문상품 리스트 + 검색"
     }
-    res.render('user', data);
+    res.render('orderItem', data);
 })
 // DB 초기화
 
